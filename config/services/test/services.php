10918673350aa803b516c17beaf2c8e1f941c0b3
Pieter Kuijper <pckuijper@gmail.com>
2023-02-02 17:06:20 +0100

    Update kernel to configure config loading

---
+++ config/services/test/services.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Saul\Core\Port\Persistence\PersistenceServiceInterface;
+use Saul\Test\Framework\InMemoryPersistenceService;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return function (ContainerConfigurator $container): void {
+    $services = $container
+        ->services()
+        ->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->set(PersistenceServiceInterface::class, InMemoryPersistenceService::class);
+};
--- src/Infrastructure/Framework/Symfony/Kernel.php
+++ src/Infrastructure/Framework/Symfony/Kernel.php
@@ -3,9 +3,53 @@
 namespace Saul\Infrastructure\Framework\Symfony;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    private const ENV_PROD = 'prod';
+    private const CONFIG_FILE_EXTENSIONS = '{yaml,php}';
+
+    protected function configureContainer(ContainerConfigurator $container): void
+    {
+        $configDir = $this->getConfigDir();
+
+        $this->configurePackages($container, $configDir);
+        $this->configureParameters($container, $configDir);
+        $this->configureServices($container, $configDir);
+    }
+
+    private function getConfigDir(): string
+    {
+        return $this->getProjectDir() . '/config';
+    }
+
+    private function configurePackages(ContainerConfigurator $container, string $configDir): void
+    {
+        $container->import(sprintf('%s/{packages}/*.%s', $configDir, self::CONFIG_FILE_EXTENSIONS));
+        $container->import(sprintf('%s/{packages}/%s/*.%s', $configDir, $this->getEnvironment(), self::CONFIG_FILE_EXTENSIONS));
+    }
+
+    private function configureParameters(ContainerConfigurator $container, string $configDir): void
+    {
+        $environmentList = array_unique([self::ENV_PROD, $this->getEnvironment()]);
+
+        foreach ($environmentList as $environment) {
+            $container->import(sprintf('%s/{parameters}/{%s}.%s', $configDir, $environment, self::CONFIG_FILE_EXTENSIONS));
+            $container->import(sprintf('%s/{parameters}/{%s}/**/*.%s', $configDir, $environment, self::CONFIG_FILE_EXTENSIONS));
+        }
+    }
+
+    private function configureServices(ContainerConfigurator $container, string $configDir): void
+    {
+        $environmentList = array_unique([self::ENV_PROD, $this->getEnvironment()]);
+
+        foreach ($environmentList as $environment) {
+            $container->import(sprintf('%s/{services}/{%s}.%s', $configDir, $environment, self::CONFIG_FILE_EXTENSIONS));
+            $container->import(sprintf('%s/{services}/{%s}/**/*.%s', $configDir, $environment, self::CONFIG_FILE_EXTENSIONS));
+        }
+    }
 }
