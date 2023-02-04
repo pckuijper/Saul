0cc15eb78254e7bee0f4d03f0d7f95ef7640639f
Pieter Kuijper <pckuijper@gmail.com>
2023-02-04 21:17:22 +0100

    Allow configuring the container through TestApp
    This allows us to modify the container on a per test basis so we can
    overwrite any service for a test to instead inject a fake, spy or dummy.

--- tests/Framework/TestApp.php
+++ tests/Framework/TestApp.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Saul\Test\Framework;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 final class TestApp
@@ -15,6 +17,7 @@ final class TestApp
     private KernelBrowser $client;
     private KernelInterface $kernel;
     private Application $cliApp;
+    private ContainerInterface $container;
 
     public function __construct()
     {
@@ -30,6 +33,8 @@ final class TestApp
         $this->client = $webTestCase->extractClient();
         $this->client->catchExceptions(true);
         $this->client->disableReboot();
+
+        $this->container = $this->getContainer();
     }
 
     /**
@@ -43,10 +48,7 @@ final class TestApp
         array $inputs = [],
         array $options = []
     ): CommandOutput {
-        $this->cliApp = $this->cliApp ?? new Application(
-            $this->getKernel()
-        );
-
+        $this->cliApp = $this->cliApp ?? new Application($this->getKernel());
         $commandTester = new CommandTester($this->cliApp->find($commandName));
         $commandTester->setInputs($inputs);
         $commandTester->execute($arguments, $options);
@@ -57,8 +59,26 @@ final class TestApp
         );
     }
 
+    public function setContainerService(string $classFqcn, object $object): void
+    {
+        $this->container->set($classFqcn, $object);
+    }
+
     private function getKernel(): KernelInterface
     {
         return $this->kernel ?? $this->kernel = $this->client->getKernel();
+    }
+
+    private function getContainer(): ContainerInterface
+    {
+        /** @var ?ContainerInterface */
+        $container = $this->container
+            ?? $this->client->getContainer()->get('test.service_container');
+
+        if ($container === null) {
+            throw new Exception();
+        }
+
+        return $this->container = $container;
     }
 }
