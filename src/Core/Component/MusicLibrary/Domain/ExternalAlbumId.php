f1b6dbe85834d860f38b98e3e554115bedd9f9cb
Pieter Kuijper <pckuijper@gmail.com>
2023-02-04 23:38:00 +0100

    Implement Stringable for ExternalAlbumId

--- src/Core/Component/MusicLibrary/Domain/ExternalAlbumId.php
+++ src/Core/Component/MusicLibrary/Domain/ExternalAlbumId.php
@@ -4,10 +4,17 @@ declare(strict_types=1);
 
 namespace Saul\Core\Component\MusicLibrary\Domain;
 
-final class ExternalAlbumId
+use Stringable;
+
+final class ExternalAlbumId implements Stringable
 {
     public function __construct(
         public readonly string $id
     ) {
+    }
+
+    public function __toString(): string
+    {
+        return $this->id;
     }
 }
